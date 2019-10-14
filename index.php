596f0bf8098e1d7f71cd35d8bc14cbc5b9718ae7
Jules G <dev@julesgdn.fr>
2019-10-14 11:30:33 +0200

    Déplace les templates en dehors de static pour mieux respecter l'architecture MVC

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include 'vendor/autoload.php';
 
 try {
-    $loader = new Twig_Loader_Filesystem('static/templates');
+    $loader = new Twig_Loader_Filesystem('templates');
     $twig = new Twig_Environment($loader);
     $template = $twig->loadTemplate('Homepage.html');
     echo $template->render(array());
