2ae8f50aaddd490ff4d2ff0b77053e04827dd689
Jules G <dev@julesgdn.fr>
2019-10-14 17:12:10 +0200

    Modification du fonctionnement de la liste des genres : passage des valeurs via Twig.

--- addMovie.php
+++ addMovie.php
@@ -6,7 +6,12 @@ try {
     $loader = new Twig_Loader_Filesystem('templates');
     $twig = new Twig_Environment($loader);
     $template = $twig->loadTemplate('AdditionPage.html');
-    echo $template->render(array());
+
+    $genres = ["Action", "Animation", "Aventure", "Catastrophe", "Comedie", "Documentaire", "Drame", "Fantaisie", "Horreur", "Science-fiction", "Western"];
+
+    echo $template->render(array(
+        'genres' => $genres
+    ));
 } catch (Exception $e) {
     die('ERROR: ' . $e->getMessage());
 }
